f3681e4f3fa4c2b9b38357f8496756e61dc78aa4
Park,JungHwan <park930919@gmail.com>
2016-05-13 13:58:05 +0900

    Project First Update

---
+++ Assets/Scripts/CharacterState.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum CharacterState
+{
+    None,
+
+    Idle,
+    Walk,
+    Run,
+    Dead,
+    Jump,
+    Attack,
+
+    Max,
+}
---
+++ Assets/Scripts/PlayerFSM.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFSM : FSMBase {
+
+    public int maxHP = 100;
+    public int curHP;
+    public float attack;
+    public float moveSpeed;
+    public float jumpSpeed;
+
+    public Animation waeapon;
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if (h != 0 || v != 0)
+        {
+            SetState(CharacterState.Walk);
+            Vector3 dir = new Vector3(-v, 0f, h);
+            _cc.Move(moveSpeed * dir * Time.deltaTime);
+        }
+        else
+        {
+            SetState(CharacterState.Idle);
+        }
+    }
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        curHP = maxHP;
+    }
+    protected override IEnumerator Idle()
+    {
+        while (_isNewState)
+        {
+            yield return null;
+        }
+    }
+    protected virtual IEnumerator Walk()
+    {
+        while(_isNewState)
+        {
+            waeapon.Play("Weapon");
+            yield return null;
+        }
+    }
+}
