d36b35abba41bf999d010d028acae0231d8ddf36
Park,JungHwan <park930919@gmail.com>
2016-05-23 15:31:25 +0900

    modify and Enemy add

--- Assets/Scripts/CharacterState.cs
+++ Assets/Scripts/CharacterState.cs
@@ -11,6 +11,6 @@ public enum CharacterState
     Dead,
     Jump,
     Attack,
-
+    Hit,
     Max,
 }
--- Assets/Scripts/PlayerFSM.cs
+++ Assets/Scripts/PlayerFSM.cs
@@ -5,7 +5,7 @@ public class PlayerFSM : FSMBase
 {
 
     public int maxHP = 100;
-    public int curHP;
+    public int currentHp;
     public float attack;
     public float moveSpeed;
     public float jumpSpeed;
@@ -13,30 +13,28 @@ public class PlayerFSM : FSMBase
     private Vector3 dir;
     public Animation waeapon;
 
-    
+
     private float h;
     private float v;
     void Update()
     {
+        if (IsDead())
+            return;
         h = Input.GetAxis("Horizontal");
         v = Input.GetAxis("Vertical");
         //Move Rogic
-
-        if(state == CharacterState.Attack)
+        if (state == CharacterState.Attack)
         {
             v = 0;
-            h = transform.rotation.y !=0 ? -0.5f : 0.5f;
+            h = transform.rotation.y != 0 ? -0.5f : 0.5f;
+        }
+        if (state == CharacterState.Hit)
+        {
+            v = 0;
+            h = transform.rotation.y != 0 ? 0.3f : -0.3f;
         }
         dir = new Vector3(moveSpeed * -v, dir.y, moveSpeed * h);
-        if (_cc.isGrounded)
-        {
-            dir.y = -6f;
-            if (Input.GetButtonDown("Jump") && state != CharacterState.Attack && state != CharacterState.Jump)
-            {
-                dir.y = jumpSpeed;
-                SetState(CharacterState.Jump);
-            }
-        }
+
         dir.y -= gravity * Time.deltaTime;
         _cc.Move(dir * Time.deltaTime);
 
@@ -54,7 +52,7 @@ public class PlayerFSM : FSMBase
     protected override void OnEnable()
     {
         base.OnEnable();
-        curHP = maxHP;
+        currentHp = maxHP;
     }
     protected override IEnumerator Idle()
     {
@@ -64,11 +62,23 @@ public class PlayerFSM : FSMBase
             if (h != 0 || v != 0)
             {
                 SetState(CharacterState.Walk);
+                break;
             }
 
             if (Input.GetButtonDown("Fire1"))
             {
                 SetState(CharacterState.Attack);
+                break;
+            }
+            if (_cc.isGrounded)
+            {
+                dir.y = -6f;
+                if (Input.GetButtonDown("Jump") && state != CharacterState.Attack && state != CharacterState.Jump)
+                {
+                    dir.y = jumpSpeed;
+                    SetState(CharacterState.Jump);
+                    break;
+                }
             }
         }
     }
@@ -80,11 +90,23 @@ public class PlayerFSM : FSMBase
             if (h == 0 && v == 0)
             {
                 SetState(CharacterState.Idle);
+                break;
             }
 
             if (Input.GetButtonDown("Fire1"))
             {
                 SetState(CharacterState.Attack);
+                break;
+            }
+            if (_cc.isGrounded)
+            {
+                dir.y = -6f;
+                if (Input.GetButtonDown("Jump") && state != CharacterState.Attack && state != CharacterState.Jump)
+                {
+                    dir.y = jumpSpeed;
+                    SetState(CharacterState.Jump);
+                    break;
+                }
             }
         }
     }
@@ -93,24 +115,67 @@ public class PlayerFSM : FSMBase
         while (!_isNewState)
         {
             yield return null;
-            if (_cc.isGrounded && _a.GetCurrentAnimatorStateInfo(0).normalizedTime %1 >0.2 && _a.GetCurrentAnimatorStateInfo(0).IsName("overlordLand"))
+            if (_cc.isGrounded && _a.GetCurrentAnimatorStateInfo(0).normalizedTime % 1 > 0.2 && _a.GetCurrentAnimatorStateInfo(0).IsName("overlordLand"))
             {
                 if (h == 0 && v == 0)
+                {
                     SetState(CharacterState.Idle);
+                    break;
+                }
                 else
+                {
                     SetState(CharacterState.Walk);
+                    break;
+                }
             }
         }
     }
     protected virtual IEnumerator Attack()
     {
-        while(!_isNewState)
+        while (!_isNewState)
         {
             yield return null;
-            if (_a.GetCurrentAnimatorStateInfo(0).IsName("Attack") && _a.GetCurrentAnimatorStateInfo(0).normalizedTime %1 >0.9)
+            if (_a.GetCurrentAnimatorStateInfo(0).IsName("Attack") && _a.GetCurrentAnimatorStateInfo(0).normalizedTime % 1 > 0.9)
             {
                 SetState(CharacterState.Idle);
+                break;
             }
         }
+    }
+    protected virtual IEnumerator Hit()
+    {
+        float _t = 0;
+        while (!_isNewState)
+        {
+            yield return null;
+            _t += Time.deltaTime;
+            if (currentHp <= 0)
+            {
+                currentHp = 0;
+                SetState(CharacterState.Dead);
+                break;
+            }
+            if (_t > 0.5)
+            {
+                SetState(CharacterState.Idle);
+                break;
+            }
+        }
+    }
+    protected virtual IEnumerator Dead()
+    {
+        while (!_isNewState)
+        {
+            yield return null;
+        }
+    }
+    public void SendAttack(EenemyFSM taget)
+    {
+        taget.ProcessDamage(attack);
+    }
+    public void ProcessDamage(float damage)
+    {
+        currentHp -= (int)damage;
+        SetState(CharacterState.Hit);
     }
 }
